92f0e91f7bce920d6da11eb18726d552029b7d7e
josephlarrivy <joseph.larrivy@gmail.com>
2022-10-09 17:43:03 -0500

    initial setup complete, server startup problem

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-"""Blogly application."""
+from flask import Flask, request, render_template, redirect
+from models import db, connect_db, User
+from flask_debugtoolbar import DebugToolbarExtension
 
-from flask import Flask
-from models import db, connect_db
 
 app = Flask(__name__)
+if __name__ == '__main__':
+    app.run()
+
+
+app.config['SECRET_KEY'] = "asoihfbasjbdfsadf"
+
+debug = DebugToolbarExtension(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
+
+
+@app.route("/")
+def list_users():
+    """lists all users in DB"""
+    users = User.query.all()
+    return render_template("users.html", users=users)
+
+
+@app.route('/add_user', methods=["POST"])
+def add_user():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    url = request.form['url']
+
+    user = User(first_name=first_name, last_name=last_name, url=url)
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect(f"/{user.id}")
--- models.py
+++ models.py
@@ -1,1 +1,25 @@
-"""Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User (db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    url = db.Column(db.String)
+
+    @property
+    def full_name(self):
+        """Return full name of user."""
+        return f"{self.first_name} {self.last_name}"
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
